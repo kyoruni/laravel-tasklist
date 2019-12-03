17366765c4fa8eac32e9134b9c18ca047f86d974
kyoruni <kyoruni@gmail.com>
2019-12-03 22:31:47 +0900

    :wrench: テストデータ作成

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -11,6 +11,32 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \DB::table('tasks')->insert([
+            [
+                'content' => 'ゴミ捨て',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'content' => 'お風呂掃除',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'content' => '洗濯',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'content' => '買い物',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'content' => '振り込み',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ]);
     }
 }
