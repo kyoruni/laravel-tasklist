cd4f19149fbfe989e310775023a3e4da022f48c9
kyoruni <kyoruni@gmail.com>
2019-12-08 20:31:18 +0900

    :wrench: 仮の一覧ページ作成

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class TasksController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('tasks.index');
+    }
 }
