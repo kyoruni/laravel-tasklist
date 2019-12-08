ddc422bc6c5674db51dc9bd87cb0c3ee5a112145
kyoruni <kyoruni@gmail.com>
2019-12-08 22:01:52 +0900

    :wrench: task一覧表示

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    @foreach ($tasks as $task)
-        {{ $task->content }}
-    @endforeach
+    <table class="table table-bordered">
+        <thead class="thead-light">
+            <tr>
+                <th>No.</th>
+                <th scole="col">タスク</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tasks as $task)
+                <tr>
+                    <th scope="row">{{ $task->id }}</th>
+                    <td>{{ $task->content }}</td>
+                </tr>
+            @endforeach
+        <tbody>
+    </table>
 @endsection
